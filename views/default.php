3a8bd823a36436aa8a1cd56628fce036e6a10b06
Skwady <yohann.vaslot@gmail.com>
2025-01-14 21:29:34 +0100

    setup meta desc

--- views/default.php
+++ views/default.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Arcadia est un zoo dans la fôret de Brocéliande en bretagne venez découvrir nos habitats et leurs animaux">
+    <meta name="description" content="Les Pattes vers l'Avenir est une association de protection animale qui propose des adoptions de chiens et chats, une pension pour animaux et des missions de bénévolat.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
